31b75adbd742caec18bc38ef30f6e92dd721c33a
Brian Jacobel <bjacobel@gmail.com>
2019-01-06 17:27:21 -0500

    Grafana is working with a userdir chown

--- ecs/user-data.sh
+++ ecs/user-data.sh
@@ -8,3 +8,4 @@ mkdir -p /efs
 mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 \
     ${efs_dns_name}:/ /efs
 sudo chown -R 500:500 /efs
+sudo chown -R 472:472 /efs/grafana
